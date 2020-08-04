ab6979119c79d0f815a3fc5bee75a7cc2485acd9
Vinicius Imai <vinicius.imai@gmail.com>
2020-08-03 21:36:29 -0300

    feature - sign up and dashboard page

--- users/views.py
+++ users/views.py
@@ -1,10 +1,32 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.models import User
+from django.contrib import auth
 
 # from .models import Users
 
 
-def signup(request, post_id):
-    pass
+def signup(request):
+    if request.method == 'POST':
+        print('user')
+        email = request.POST['email']
+        password = request.POST['password']
+        print(email, password)
+        if not email.strip():
+            return redirect('signup')
+        if not password.strip():
+            return redirect('signup')
+
+        if User.objects.filter(email=email).exists():
+            return redirect('signup')
+
+        user = User.objects.create_user(email=email, password=password)
+        user.save()
+
+        return redirect('login')
+    else:
+        print('get')
+
+    return render(request, 'users/signup.html')
 
 
 def login(request, post_id):
@@ -16,16 +38,10 @@ def login(request, post_id):
 
 
 def logout(request, post_id):
-    # post_object = get_object_or_404(Post, pk=post_id)
-    # data = {
-    #     'post': post_object
-    # }
+
     return render(request, 'post.html')
 
 
-def dashboard(request, post_id):
-    # post_object = get_object_or_404(Post, pk=post_id)
-    # data = {
-    #     'post': post_object
-    # }
-    return render(request, 'post.html')
+def dashboard(request):
+    print('ddashboard')
+    return render(request, 'users/dashboard.html')
