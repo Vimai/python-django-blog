7e580fa7b6911cb790bca66b593f489f460904f4
Vinicius Imai <vinicius.imai@gmail.com>
2020-08-03 21:36:45 -0300

    feature - login page

--- users/views.py
+++ users/views.py
@@ -29,12 +29,25 @@ def signup(request):
     return render(request, 'users/signup.html')
 
 
-def login(request, post_id):
-    # post_object = get_object_or_404(Post, pk=post_id)
-    # data = {
-    #     'post': post_object
-    # }
-    return render(request, 'post.html')
+def login(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+        if not email.strip():
+            return redirect('login')
+        if not password.strip():
+            return redirect('login')
+
+        if User.objects.filter(email=email).exists():
+            user_name = User.objects.filter(email=email).values_list('username', flat=True).get()
+            user = auth.authenticate(request, username=user_name, password=password)
+            if user:
+                auth.login(request, user)
+                return redirect('dashboard')
+            else:
+                return redirect('login')
+
+    return render(request, 'users/login.html')
 
 
 def logout(request, post_id):
